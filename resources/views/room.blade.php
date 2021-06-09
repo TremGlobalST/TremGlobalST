bee68e3e42fc8ea79518ebb03c466d7c2010e4ec
onuraltinsoy <onuraltinsoy80@gmail.com>
2021-06-09 15:30:06 +0300

    fullscreen

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -6,11 +6,24 @@
     .ytp-show-cards-title {
         display: none;
     }
+    .ofs{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        opacity: 0;
+        border: none;
+        background: #dedede;
+        box-shadow: 2px 2px 6px 3px #ccc;
+        padding: 20px;
+        font-weight: bold;
+        font-size: 22px;
+        color: gray;
+    }
 </style>
 
 @section('content')
     <div id="app" class="bg">
-        <div class="wrapper">
+        <div class="wrapper" onclick="toggleBtns()">
             <span class="date" v-if="whileMeet || startingMeet">
                 Saat: @{{ date }}
             </span>
@@ -30,7 +43,7 @@
                 <span class="time">@{{ meetStart }} - @{{ meetEnd }}</span>
             </div>
 
-            <div class="video-wrapper" v-if="noMeet">
+            <div class="video-wrapper" v-if="noMeet" style="height:80%">
                 <video width="auto" height="100%"  autoplay muted loop>
                     <source src="/videos/ad.mp4" type="video/mp4">
                     Your browser does not support the video tag.
@@ -40,6 +53,7 @@
             <div class="footer">
                 <img src="/images/torlogo.svg" alt="">
             </div>
+            <button class="ofs" onclick="toggleFullscreen(event)">fullscreen</button>
         </div>
     </div>
 @endsection
@@ -50,4 +64,53 @@
     <script>
         const roomId = {{ $room->id }};
     </script>
+    <script>
+        /* Get the documentElement (<html>) to display the page in fullscreen */
+        var elem = document.documentElement;
+
+        var showBtn = false;
+        var isFull = false;
+
+        function toggleBtns() {
+            if (showBtn) {
+                document.getElementsByClassName('ofs')[0].style.opacity = 0;
+            } else {
+                document.getElementsByClassName('ofs')[0].style.opacity = 1;
+            }
+            showBtn = !showBtn;
+        }
+
+        function toggleFullscreen(e) {
+            if (isFull) {
+                closeFullscreen(e);
+            } else {
+                openFullscreen(e);
+            }
+            isFull = !isFull;
+        }
+        
+        /* View in fullscreen */
+        function openFullscreen(e) {
+            e.preventDefault();
+          if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+          } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+          } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+          }
+        }
+        
+        /* Close fullscreen */
+        function closeFullscreen(e) {
+            e.preventDefault();
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+          }
+        }
+        </script>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,6 +173,19 @@
             width: 100%;
             height: calc(100% + 120px);
         }
+        .ofs{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        opacity: 0;
+        border: none;
+        background: #dedede;
+        box-shadow: 2px 2px 6px 3px #ccc;
+        padding: 20px;
+        font-weight: bold;
+        font-size: 22px;
+        color: gray;
+    }
 
         @media (max-width: 1680px) {
           #logo-wrapper {
@@ -183,6 +196,7 @@
 @endsection
 
 @section('content')
+<div onclick="toggleBtns()">
 <img src="/images/bo.svg" id="bo">
 <div id="logo-wrapper">
   <img src="/images/logos.svg" class="invest">
@@ -200,6 +214,9 @@
           <div id="player"></div>
       </div>
   </div>
+  
+</div>
+<button class="ofs" onclick="toggleFullscreen(event)">fullscreen</button>
 </div>
 @endsection
 
@@ -276,4 +293,53 @@
         }
 
     </script>
+    <script>
+      /* Get the documentElement (<html>) to display the page in fullscreen */
+      var elem = document.documentElement;
+
+      var showBtn = false;
+      var isFull = false;
+
+      function toggleBtns() {
+          if (showBtn) {
+              document.getElementsByClassName('ofs')[0].style.opacity = 0;
+          } else {
+              document.getElementsByClassName('ofs')[0].style.opacity = 1;
+          }
+          showBtn = !showBtn;
+      }
+
+      function toggleFullscreen(e) {
+          if (isFull) {
+              closeFullscreen(e);
+          } else {
+              openFullscreen(e);
+          }
+          isFull = !isFull;
+      }
+      
+      /* View in fullscreen */
+      function openFullscreen(e) {
+          e.preventDefault();
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+          elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+          elem.msRequestFullscreen();
+        }
+      }
+      
+      /* Close fullscreen */
+      function closeFullscreen(e) {
+          e.preventDefault();
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+          document.msExitFullscreen();
+        }
+      }
+      </script>
 @endsection
